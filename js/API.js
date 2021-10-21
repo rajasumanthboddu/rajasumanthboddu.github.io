3c27ca504c5cb838dc44ea2f298b9a912e9d960d
rajasumanthboddu <65333645+rajasumanthboddu@users.noreply.github.com>
2021-10-21 16:37:20 +0530

    Add files via upload

--- js/API.js
+++ js/API.js
@@ -1,52 +1,52 @@
-// document.querySelector("div.page-wrapper.bg-gra-02.p-t-13.p-b-10.font-poppins").addEventListener('click', function(e){
-//   console.log("aa")
-//   document.querySelector('#input').addEventListener('input', function(f){
-//       console.log(f.target.value);
-//   var data = JSON.stringify({
-//       "parentIntent": "",
-//       "input": f.target.value,
-//       "streamName": "HR Assist.ai"
-//     });
+document.querySelectorAll("button")[0].addEventListener('click', function(e){
+    event.preventDefault();
+  console.log(document.querySelectorAll('input')[0].value)
+    var data = JSON.stringify({
+        "parentIntent": "",
+        "input": document.querySelectorAll('input')[0].value,
+        "streamName": "HR Assist.ai"
+        });
     
-//     var xhr = new XMLHttpRequest();
-//     xhr.withCredentials = true;
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
     
-//     xhr.addEventListener("readystatechange", function() {
-//       if(this.readyState === 4) {
-//         console.log(this.responseText);
-//       }
-//     });
+    xhr.addEventListener("readystatechange", function() {
+      if(this.readyState === 4) {
+        console.log(this.responseText);
+      }
+    });
     
-//     xhr.open("POST", "https://bots.kore.ai/api/v1.1/rest/streams/st-10356dfe-12f1-5bbc-bbc3-4aa414912640/findIntent?fetchConfiguredTasks=false");
-//     xhr.setRequestHeader("Content-Type", "application/json");
-//     xhr.setRequestHeader("auth", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6ImNzLWY4MTQ4NTY2LTNlNjgtNTIzNC05NDhmLTdmZjdmZjI2ZTA4NSJ9.95y60Y2tdYLzii3HPXmyy9IDv6qHBNQNP73cbIWsEI4");
-//     xhr.setRequestHeader("bot-language", "en");
+    xhr.open("POST", "https://bots.kore.ai/api/v1.1/rest/streams/st-10356dfe-12f1-5bbc-bbc3-4aa414912640/findIntent?fetchConfiguredTasks=false");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("auth", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6ImNzLWY4MTQ4NTY2LTNlNjgtNTIzNC05NDhmLTdmZjdmZjI2ZTA4NSJ9.95y60Y2tdYLzii3HPXmyy9IDv6qHBNQNP73cbIWsEI4");
+    xhr.setRequestHeader("bot-language", "en");
     
-//     xhr.send(data);
-// })})
+    xhr.send(data);
+    // document.querySelectorAll('input')[1].value = 
+})
 
 
-var data = JSON.stringify({
-  "parentIntent": "",
-  "input": "Apply Leave",
-  "streamName": "HR Assist.ai"
-});
+// var data = JSON.stringify({
+//   "parentIntent": "",
+//   "input": "Apply Leave",
+//   "streamName": "HR Assist.ai"
+// });
 
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
+// var xhr = new XMLHttpRequest();
+// xhr.withCredentials = true;
 
-xhr.addEventListener("readystatechange", function() {
-  if(this.readyState === 4) {
-    console.log(this.responseText);
-  }
-});
+// xhr.addEventListener("readystatechange", function() {
+//   if(this.readyState === 4) {
+//     console.log(this.responseText);
+//   }
+// });
 
-xhr.open("POST", "https://bots.kore.ai/api/v1.1/rest/streams/st-10356dfe-12f1-5bbc-bbc3-4aa414912640/findIntent?fetchConfiguredTasks=false");
-xhr.setRequestHeader("Content-Type", "application/json");
-xhr.setRequestHeader("auth", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6ImNzLWY4MTQ4NTY2LTNlNjgtNTIzNC05NDhmLTdmZjdmZjI2ZTA4NSJ9.95y60Y2tdYLzii3HPXmyy9IDv6qHBNQNP73cbIWsEI4");
-xhr.setRequestHeader("bot-language", "en");
+// xhr.open("POST", "https://bots.kore.ai/api/v1.1/rest/streams/st-10356dfe-12f1-5bbc-bbc3-4aa414912640/findIntent?fetchConfiguredTasks=false");
+// xhr.setRequestHeader("Content-Type", "application/json");
+// xhr.setRequestHeader("auth", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBJZCI6ImNzLWY4MTQ4NTY2LTNlNjgtNTIzNC05NDhmLTdmZjdmZjI2ZTA4NSJ9.95y60Y2tdYLzii3HPXmyy9IDv6qHBNQNP73cbIWsEI4");
+// xhr.setRequestHeader("bot-language", "en");
 
-xhr.send(data);
+// xhr.send(data);
 
 // var request = require('request');
 // var options = {
